0dd2276020bb2b8404bdf078351f3dd5c7e2aef7
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2024-10-30 16:18:28 +0530

    eb&et

---
+++ html.js
@@ -0,0 +1,59 @@
+// // var a=10;
+// // a=200;
+// // a=true;
+
+// // function a1(){
+// //     var a=20;
+// //     var b=30;
+// //     var c=a+b;
+// //     console.log(c)
+// // }
+// // a1()
+
+// function a1(){
+//     var a=20;
+//     var b=30;
+//     var c=a+b;
+//     console.log(c)
+// }
+
+// // console.log(c)
+// function a2(){
+//     var a=20;
+//     var b=30;
+//     var c=a+b;
+//     console.log(c)
+// }
+// a2()
+// a2()
+// a2()
+// a2()
+// a2()
+
+
+// // function a1(){
+// // console.log("hello");
+
+// // }
+// // a1()
+
+
+var a;  //undefined
+a=1000;
+var a=300;
+var a=300000;
+
+
+// let z;
+// z="vamsi"
+// z="ravi"
+// z="1234567890-"
+// let z=3000;
+
+const w=3000;
+// const w="vamsi"
+w="wertyuiop"
+console.log(w)
+// w=3000;
+// const w;
+
